80fec426345789506cfcf4f55d76ef8956f48609
JeongSeoyun <xeonj@naver.com>
2024-09-19 14:17:43 +0900

    chore: prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App" />
-  );
+  return <div className="App" />
 }
 
-export default App;
+export default App
