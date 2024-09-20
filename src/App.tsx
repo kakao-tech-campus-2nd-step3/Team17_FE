5dd328fc85acd22493f615a4d82377e864e68c87
leedohyun <leedo573@gmail.com>
2024-09-20 18:22:09 +0900

    기본세팅 및 로그인 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div className="App" />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
