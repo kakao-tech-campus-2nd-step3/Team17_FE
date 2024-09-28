1083dc46258921714101addb9f27acb8da19796b
JeongSeoyun <xeonj@naver.com>
2024-09-28 18:26:36 +0900

    chore: 메인페이지 목업데이터 추가

---
+++ src/mocks/MainMock.ts
@@ -0,0 +1,24 @@
+const mainMock = {
+    totalTime: "Time",
+    exerciseList: [
+        {
+            exerciseId: 1,  // Long
+            exerciseName: "죽음의 타바타",  // String
+            exerciseTime: 600000, // Long 타입의 시간 (밀리초 단위)
+            isActive: true  // boolean
+        },
+        {
+            exerciseId: 2,
+            exerciseName: "아침 스트레칭",
+            exerciseTime: 600000,
+            isActive: true
+        }
+    ],
+    diary: [
+        {
+            memo: "오늘은 아침에 일어나서 계획한 대로 운동을 했다. 너무 뿌듯하다. 친구들과 함께 운동하니 운동 의욕이 커지는 것 같다."
+        }
+    ]
+};
+
+export default mainMock
