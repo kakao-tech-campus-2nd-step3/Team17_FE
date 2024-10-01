0a0c0b642b96be46b99be81e87ec1c9d57c02222
JeongSeoyun <xeonj@naver.com>
2024-10-01 21:29:32 +0900

    feat: 메인페이지 홈트일기 작성 컴포넌트 및 기능 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-console */
 import styled from '@emotion/styled'
 import { useState } from 'react'
 import Timer from '../components/Timer'
 import ExerciseList from '../components/ExerciseList'
 import mainMock from '../mocks/MainMock'
+import DiaryCreate from '../components/DiaryCreate'
+import axiosInstance from '../api/axiosInstance'
 
 const Main = () => {
 
@@ -12,6 +15,20 @@ const Main = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
   const isAnyActive = exerciseList.some(exercise => exercise.isActive)
 
+  const [newDiary, setNewDiary] = useState("")
+
+  const handleDiarySubmit = async () => {
+    try {
+      const response = await axiosInstance.post('/api/diary', {
+        memo: newDiary
+      })
+      console.log('Diary 전송 성공', response.data)
+      setNewDiary('')
+    } catch(error) {
+      console.error('Error submitting diary:', error)
+    }
+  }
+
   return (
     <MainWrapper>
         <DateContainer>
@@ -19,6 +36,9 @@ const Main = () => {
         </DateContainer>
         <Container>
           <ExerciseList selectedDate={selectedDate} exerciseList={exerciseList} setTotalTime={setTotalTime} setExerciseList={setExerciseList} />
+        </Container>
+        <Container>
+          <DiaryCreate newDiary={newDiary} setNewDiary={setNewDiary} onSubmit={handleDiarySubmit} />
         </Container>
     </MainWrapper>
   )
