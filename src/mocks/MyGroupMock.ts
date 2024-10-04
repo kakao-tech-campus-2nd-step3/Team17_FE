9638cafbe49ddeef03f32e8481678649363fcb99
leedohyun <leedo573@gmail.com>
2024-10-04 09:41:25 +0900

    feat: 그룹 탐색 페이지 전반적 ui 구현

---
+++ src/mocks/MyGroupMock.ts
@@ -0,0 +1,107 @@
+const myGroupMock = {
+    groupList : [
+    {
+        groupId : 1,
+        groupName : "매.운.도",
+        leaderNickname : "최강운동",
+        maxParticipants : 4,
+        tagList : [
+            { tagName : "여성" },
+            { tagName : "20대" },
+            { tagNmae : "강" }
+        ]
+    },
+    {
+        groupId : 2,
+        groupName : "숨쉬기 운동 도전",
+        leaderNickname : "운동하는 운동짐",
+        maxParticipants : 4,
+        tagList : [
+            { tagName : "남성" },
+            { tagName : "20대" },
+            { tagNmae : "30대" },
+            { tagName : "강" }
+        ]
+    },
+    {
+        groupId : 3,
+        groupName : "모각트",
+        leaderNickname : "myName",
+        maxParticipants : 4,
+        tagList : [
+            { tagName : "여성" },
+            { tagName : "남성" },
+            { tagName : "전연령" },
+            { tagName : "자유" }
+        ],
+    },
+    {
+        groupId : 4,
+        groupName : "런앤번",
+        leaderNickname : "러닝러닝",
+        maxParticipants : 4,
+        tagList : [
+            { tagName : "무관" }
+        ],
+    },
+    {
+        groupId : 5,
+        groupName : "매일 운동 도전",
+        leaderNickname : "러닝맨",
+        maxParticipants : 6,
+        tagList : [
+            { tagName : "무관" },
+            { tagName : "자유" }
+        ],
+    },
+    {
+        groupId : 6,
+        groupName : "내일 운동 도전",
+        leaderNickname : "운동내일",
+        maxParticipants : 1,
+        tagList : [
+            { tagName : "무관" },
+            { tagName : "20대" },
+            { tagName : "30대" },
+            { tagName : "40대" },
+            { tagName : "약" }
+        ], 
+    },
+    {
+        groupId : 7,
+        groupName : "하이핏",
+        leaderNickname : "아임파인",
+        maxParticipants : 7,
+        tagList : [
+            { tagName : "여성" },
+            { tagName : "20대" },
+            { tagName : "약" },
+            { tagName : "중" }
+        ],
+    },
+    {
+        groupId : 8,
+        groupName : "홈트라이",
+        leaderNickname : "에브리러닝",
+        maxParticipants : 13,
+        tagList : [
+            { tagName : "여성" },
+            { tagName : "20대" },
+            { tagName : "30대" },
+            { tagName : "중" },
+            { tagName : "약" }
+        ],
+    },
+    {
+        groupId : 9,
+        groupName : "오운완",
+        leaderNickname : "캔유러닝",
+        maxParticipants : 4,
+        tagList : [
+            { tagName : "무관"}
+        ],
+    }
+]
+};
+
+export default myGroupMock
