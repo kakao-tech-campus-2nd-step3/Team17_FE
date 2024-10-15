0316a8c87cc159772d946c7d49219d1f63eb490d
JeongSeoyun <xeonj@naver.com>
2024-10-15 14:03:44 +0900

    chore: baseurl 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: '',
+  baseURL: 'https://home-try.13.125.102.156.sslip.io',
   timeout: 20000,
   headers: { 'Content-Type': 'application/json' },
 })
